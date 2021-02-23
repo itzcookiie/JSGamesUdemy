bbfa247771b249ed3d62f5b53e09a145ce7dae66
itzcookiie <mayowaadeniyi@hotmail.com>
2021-02-23 03:28:15 +0000

    refactor

--- functionator/index.js
+++ functionator/index.js
@@ -1,11 +1,8 @@
 const actionsContainer = document.createElement('div'),
 movingObjectContainer = document.createElement('div');
 
+const distance = 100;
 const actions = [];
-const position = {
-    top: 0,
-    left: 0
-};
 
 document.addEventListener('DOMContentLoaded', () => {
     const fragment = document.createDocumentFragment();
@@ -35,47 +32,57 @@ function handleKeyboard(key) {
             movingObjectContainer.style.backgroundColor = `rgb(${randomNumGenerator()}, ${randomNumGenerator()}, ${randomNumGenerator()})`;
             break;
         case 'ArrowUp': {
-            return moveCommand(actionElement, 'up', 'top', -100);
+            return moveCommand(actionElement, 'up', 'top', -distance);
         }
         case 'ArrowRight': {
-            return moveCommand(actionElement, 'right', 'left', 100);
+            return moveCommand(actionElement, 'right', 'left', distance);
         }
         case 'ArrowDown': {
-            return moveCommand(actionElement, 'down', 'top', 100);
+            return moveCommand(actionElement, 'down', 'top', distance);
         }
         case 'ArrowLeft': {
-            return moveCommand(actionElement, 'left', 'left', -100);
+            return moveCommand(actionElement, 'left', 'left', -distance);
         }
         case " ": {
-            actions.forEach((action,index) => {
-                (function(time) {
-                    setTimeout(() => {
-                        actionsContainer.removeChild(action.element);
-                        action.move();
-                        actions.shift();
-                        if(!actions.length) movingObjectContainer.innerText = 'Hello World!';
-                    }, time*index);
-                })(100);
-            })
+            runActions();
         }
     }
     return null;
+}
+
+async function runActions() {
+    while(actions.length) {
+        await delay();
+    }
+}
+
+function delay() {
+    return new Promise(resolve => {
+        return (function(time) {
+            setTimeout(() => {
+                actionsContainer.removeChild(actions[0].element);
+                const action = actions.shift();
+                action.move();
+                if(!actions.length) movingObjectContainer.innerText = 'Hello World!';
+                resolve();
+            }, time);
+        })(100);
+    })
 }
 
 function randomNumGenerator() {
     return Math.floor(Math.random() * 256);
 }
 
-function moveCommand(element, direction, objectPosition, distance) {
+function moveCommand(element, direction, cssPosition, distance) {
     element.innerText = `+${direction}`;
     actionsContainer.appendChild(element);
     return {
-        direction,
         element,
         move() {
-            position[objectPosition] += distance;
-            movingObjectContainer.style[objectPosition] = position[objectPosition] + 'px';
-            movingObjectContainer.innerText = `Moving ${this.direction}`;
+            const currentPosition = parseInt(getComputedStyle(movingObjectContainer)[cssPosition]);
+            movingObjectContainer.style[cssPosition] = currentPosition + distance + 'px';
+            movingObjectContainer.innerText = `Moving ${direction}`;
         }
     }
 }
